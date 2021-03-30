f4acd15ca3b7d57996da70aed04d0f6e90904236
cannin <cannin@gmail.com>
2021-03-30 15:30:09 -0400

    Added pathway specific model value into summary

--- summarize_results.R
+++ summarize_results.R
@@ -19,6 +19,11 @@ results <- data.frame(pw_pval_true=numeric(0),
                       cor_true_pred=numeric(0),
                       cor_true_pred_base=numeric(0),
                       
+                      cor_pw_true_init=numeric(0),
+                      cor_pw_true_pred=numeric(0),
+                      cor_pw_true_pred_base=numeric(0),
+                      cor_pw_is_final_better=logical(0), 
+                      
                       seed=numeric(0),
                       
                       stringsAsFactors=FALSE)
@@ -29,6 +34,11 @@ for(i in 1:length(files)) {
   y_init <- x %*% beta_init %>% as.vector
   y_pred <- x %*% beta_final %>% as.vector
   y_pred_base <- x %*% beta_final_base %>% as.vector
+  
+  pw_val_true <- x[, genes_selected_features] %*% beta_true[genes_selected_features]
+  pw_val_init <- x[, genes_selected_features] %*% beta_init[genes_selected_features]
+  pw_val_final <- x[, genes_selected_features] %*% beta_final[genes_selected_features]
+  pw_val_final_base <- x[, genes_selected_features] %*% beta_final_base[genes_selected_features]
   
   tmp_results <- data.frame(
     pw_pval_true=gsea_true$pval[gsea_true$pathway == "Cell Cycle"],
@@ -45,10 +55,15 @@ for(i in 1:length(files)) {
     cor_true_pred=cor(y_true, y_pred),
     cor_true_pred_base=cor(y_true, y_pred_base),
     
+    cor_pw_true_init=cor(val_true, val_init),
+    cor_pw_true_pred=cor(val_true, val_final),
+    cor_pw_true_pred_base=cor(val_true, val_final_base),
+    cor_pw_is_final_better=ifelse(cor(val_true, val_final) > cor(val_true, val_final_base), TRUE, FALSE), 
+    
     seed=seed,
     
     stringsAsFactors=FALSE)
-    
+
   results <- rbind(results, tmp_results)
 }
 
@@ -58,13 +73,26 @@ saveRDS(results, "run_o2_results.rds")
 results <- readRDS("run_o2_results.rds")
 results <- unique(results[, -which(names(results) %in% c("seed"))])
 
-boxplot(results$cor_true_pred, results$cor_true_pred_base, xlim=c(0, 3), ylim=c(-0.1, 1.1))
+# COR SUMMARY 
+boxplot(results$cor_true_pred, results$cor_true_pred_base, xlim=c(0, 3), ylim=c(-0.1, 1.1), main="COR")
 summary(results$cor_true_pred)
 summary(results$cor_true_pred_base)
+
+summary(results$cor_true_pred)
+summary(results$cor_true_pred_base)
+
+# PW SUMMARY
+boxplot(results$pw_pval_final, results$pw_pval_final_base, xlim=c(0, 3), ylim=c(-0.1, 1.1), main="PVAL")
+summary(results$pw_pval_final)
+summary(results$pw_pval_final_base)
 
 summary(results$pw_pval_final)
 summary(results$pw_pval_final_base)
 
 length(which(results$pw_min_final == "Cell Cycle"))
 length(which(results$pw_min_final_base == "Cell Cycle"))
+
+length(which(results$pw_min_final == "Cell Cycle" & results$cor_true_pred >= 0.7 & results$pw_pval_final <= 0.05))
+length(which(results$pw_min_final_base == "Cell Cycle" & results$cor_true_pred_base >= 0.7 & results$pw_pval_final_base <= 0.05))
+
 
